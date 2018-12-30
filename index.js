a8d61dc80528cebb124d3ceed0e9beaa6cd17ee0
Behnam Mohammadi <itten@live.com>
2018-12-30 23:31:07 +0330

    improved throttle method

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 /**
  * @author Behnam Mohammadi
- * @ref https://blog.bitsrc.io/understanding-throttling-and-debouncing-973131c1ba07
  */
 "use strict";
 
@@ -9,17 +8,19 @@ exports.__esModule = true;
 /**
  * @description throttle
  * @public
- * @version 1.0.1
+ * @version 1.0.3
  * @param {function} func
  * @param {number} time
  * @returns {function} throttled function
  */
 exports.throttle = function throttle(func, time) {
   return function() {
-    let previousCall = this.lastCall;
-    this.lastCall = Date.now();
-    if (previousCall === undefined || this.lastCall - previousCall > time)
-      func(arguments);
+    if (this.blocked) return;
+    setTimeout(() => {
+      this.blocked = false;
+    }, time);
+    this.blocked = true;
+    func(arguments);
   };
 };
 
