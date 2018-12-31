b4cffab07a2b4be7dcb84f515522de8dbb3afd51
Iman Farahi <43908633+ImanFarahi@users.noreply.github.com>
2018-12-31 11:16:16 +0330

    add function scope and Edit names
    Add function scope and Edit names 
    delay and scope are optional

--- index.js
+++ index.js
@@ -8,36 +8,50 @@ exports.__esModule = true;
 /**
  * @description throttle
  * @public
- * @version 1.0.3
- * @param {function} func
- * @param {number} time
+ * @version 1.0.4
+ * @param {function} callBack
+ * @param {number} threshhold
+ * @param {Object} scope
  * @returns {function} throttled function
  */
-exports.throttle = function throttle(func, time) {
-  return function() {
-    if (this.blocked) return;
-    setTimeout(() => {
-      this.blocked = false;
-    }, time);
-    this.blocked = true;
-    func(arguments);
-  };
+var throttle = function (callBack, threshhold, scope) {
+    threshhold || (threshhold = 250);
+    return function () {
+        var context = (scope || this), now = +new Date, args = arguments;
+        if (this.last && now < this.last + threshhold) {
+            // hold on to it
+            clearTimeout(this.timeoutId);
+            this.timeoutId = setTimeout(function () {
+                this.last = now;
+                callBack.apply(context, args);
+            }, threshhold);
+        } else {
+            this.last = now;
+            callBack.apply(context, args);
+        }
+    };
 };
+exports.throttle = throttle;
+
+
 
 /**
  * @description debounce
  * @public
- * @version 1.0.2
- * @param {function} func
- * @param {number} time
+ * @version 1.0.3
+ * @param {function} callBack
+ * @param {number} delay
+ * @param {Object} scope
  * @returns {function} debounced function
  */
-exports.debounce = function debounce(func, time) {
-  return function() {
-    clearTimeout(this.timeoutId);
-    var args = arguments;
-    this.timeoutId = setTimeout(function() {
-      func(args);
-    }, time);
-  };
+var debounce = function (callBack, delay, scope) {
+    delay || (delay = 250);
+    return function () {
+        var context = (scope || this), args = arguments;
+        clearTimeout(this.timeoutId);
+        this.timeoutId = setTimeout(function () {
+            callBack.apply(context, args);
+        }, delay);
+    };
 };
+exports.debounce = debounce;
