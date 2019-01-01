63b483a020dac191c7e90eca654f7b5ada18ea62
Behnam Mohammadi (بهنام محمدی) <itten@live.com>
2019-01-01 10:13:49 +0330

    fixed bug in throttle method

--- index.js
+++ index.js
@@ -8,18 +8,17 @@ exports.__esModule = true;
 /**
  * @description throttle
  * @public
- * @version 1.0.3
+ * @version 1.0.4
  * @param {function} func
  * @param {number} time
  * @returns {function} throttled function
  */
 exports.throttle = function throttle(func, time) {
+  var nextAllowed = 0;
   return function() {
-    if (this.blocked) return;
-    setTimeout(() => {
-      this.blocked = false;
-    }, time);
-    this.blocked = true;
+    var now = Date.now();
+    if (now < nextAllowed) return;
+    nextAllowed = now + time;
     func(arguments);
   };
 };
